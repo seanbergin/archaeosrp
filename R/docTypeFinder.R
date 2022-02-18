c7606f586dd97496967a954efe6a442372ba1564
Sean Bergin <sbergin@asu.edu>
2022-02-18 10:29:01 -0700

    type 20 work

--- R/docTypeFinder.R
+++ R/docTypeFinder.R
@@ -44,6 +44,7 @@ docTypeFinder <- function(fullString){
     if(presence){
       stringTitle = "different from original"
       presence = grepl(stringTitle,fullString)
+      cat(presence)
       if(presence){
         value <- append(value,5)
        } else {value <- append(value,20)}
